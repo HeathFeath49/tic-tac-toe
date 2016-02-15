9bdf99716b96d8c35666a819e469b7b9c3f08675
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-02-15 14:28:36 -0500

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -3,29 +3,27 @@ function createGrid(rows,cols){
 	//create table and append to body
 	var t = document.createElement("TABLE");
 	t.setAttribute("id","gameBoard");
-	document.body.appendChild(t);
+	
 
-	var alphabetIds = ["A","B","C","E","F","G","H","I"];
-	var numberIds = ["1","2","3","4","5","6","7","8"];
-	for(i=0;i<rows;i++){
+	
+	for(var i=0;i<rows;i++){
 
 		//create row and append to table
 		var r = document.createElement("TR");
-		var uniqId = alphabetIds[i];
-		r.setAttribute("id",uniqId);
-		document.getElementById("gameBoard").appendChild(r);
+		var rowId = i.toString();
+		r.setAttribute("id",rowId);
+		t.appendChild(r);
 
-		for(j=0;j<cols;j++){
+		for(var j=0;j<cols;j++){
 
 			var c = document.createElement("TD");
-			var colId = uniqId + numberIds[j];
+			var colId = rowId + j.toString();
 			c.setAttribute("id",colId);
 			c.setAttribute("class","game-grid-cell");
-			document.getElementById(uniqId).appendChild(c);
+			t.appendChild(c);
 		}
-
 	}
-
+	document.body.appendChild(t);
 }
 
 createGrid(3,3);
@@ -36,4 +34,4 @@ function setCellText(row,col,str){
 	document.getElementById(cell).appendChild(text);
 }
 
-setCellText("A","2","X");
+setCellText("2","2","X");
