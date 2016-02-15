92d1750a964aa2d08664d4be1f6ada0f87a36dce
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-02-15 13:36:29 -0500

    Update ai.js
    added copyBoard function
    no recursive call yet

--- ai.js
+++ ai.js
@@ -1,5 +1,12 @@
 //Perfect AI for Tic Tac Toe 
 //Recursive Algorithm
+function copyBoard(board){
+	var copy = [];
+	for(var i=0; i<board.length;i++){
+		copy.push(board[i]);
+	}
+	return copy;
+}
 
 function gameOver(){
 	var outcome;
@@ -21,13 +28,22 @@ function gameOver(){
 	return false
 }
 
-function minMax(gameState){
+//board holding game with all player positions or lack of
+//maxPlayer is a boolean
+
+function minMax(board,maxPlayer){
 	var check = gameOver();
 	//base case: if there is a winner or a draw
 	if(!(check === false)){
 		return check;
 	}
 	//recursion
-	
+	for(var r=0;r<=this.rows;r++){
+		for(var c=0;c<=this.cols;c++){
+			var newBoard = copyBoard(board);
+			this.makeMove()
+		}
+	}
+
 
 }
