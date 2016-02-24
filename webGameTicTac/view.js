cf1b01d3614ec2ba4ce51c7bc0b700969cba73ff
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-02-23 22:03:49 -0500

    Update view.js

--- webGameTicTac/view.js
+++ webGameTicTac/view.js
@@ -19,7 +19,7 @@ function View(model,callback){
 			var cellId = rowId + j.toString();
 			c.setAttribute("id",cellId);
 			c.setAttribute("class","game-grid-cell");
-			callback(c);
+			callback(c,i,j);
 			t.appendChild(c);
 		}
 	}
@@ -27,41 +27,12 @@ function View(model,callback){
 }
 
 
-// function createGrid(rows,cols){
+View.prototype.setCellText = function(row,col,str){
+	var text = document.createTextNode(str);
+	var cell = row.toString()+col.toString();
+	document.getElementById(cell).appendChild(text);
+}
 
-// 	//create table and append to body
-// 	var t = document.createElement("TABLE");
-// 	t.setAttribute("id","gameBoard");
-	
 
-	
-// 	for(var i=0;i<rows;i++){
 
-// 		//create row and append to table
-// 		var r = document.createElement("TR");
-// 		var rowId = i.toString();
-// 		r.setAttribute("id",rowId);
-// 		t.appendChild(r);
-
-// 		for(var j=0;j<cols;j++){
-
-// 			var c = document.createElement("TD");
-// 			var colId = rowId + j.toString();
-// 			c.setAttribute("id",colId);
-// 			c.setAttribute("class","game-grid-cell");
-// 			t.appendChild(c);
-// 		}
-// 	}
-// 	document.getElementById("boardWrap").appendChild(t);
-// }
-
-// //createGrid(3,3);
-
-// function setCellText(row,col,str){
-// 	var text = document.createTextNode(str);
-// 	var cell = row + col;
-// 	document.getElementById(cell).appendChild(text);
-// }
-
-// //setCellText("2","2","X");
 
