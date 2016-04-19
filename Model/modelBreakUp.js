fa435dcd82438b0f76974cc415a06caf5ac46829
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-04-19 13:27:35 -0400

    Finished model break up

--- Model/modelBreakUp.js
+++ Model/modelBreakUp.js
@@ -1,13 +1,96 @@
-function main(){
+"use strict";
+//main model class
+function gameRulesClass(){
 	this.myChangeListeners = [];
-}
+};
+
+gameRulesClass.prototype.notifyOfChanges = function(type){
+	for(var i=0;i< this.myChangeListeners.length;i++){
+		this.myChangeListeners[i]({change:type});
+	}
+};
+
+gameRulesClass.prototype.addChangeListeners = function(f){
+	this.myChangeListeners.push(f);
+};
+
+gameRulesClass.prototype.isDraw = function(){
+	var check = this.playerWin();
+	if((this.numOfMoves === 9) && (check === "")){
+		return true;
+	}
+	else{
+		return false;
+	}
+};
+
+gameRulesClass.prototype.isValidMove = function(row,col){
+
+	if(this.board[row][col] == ""){
+		return true;
+	}
+	else {
+		return false;
+	}
+};
+
+gameRulesClass.prototype.playerWin = function(){
+
+ 	for(var p=0;p<this.players.length;p++){
+ 		//check horizontal
+ 		if(this.board[0][0] == this.players[p] && this.board[0][1] == this.players[p] && this.board[0][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[1][0] == this.players[p] && this.board[1][1] == this.players[p] && this.board[1][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[2][0] == this.players[p] && this.board[2][1] == this.players[p] && this.board[2][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+
+ 		//check vertical
+ 		if(this.board[0][0] == this.players[p] && this.board[1][0] == this.players[p] && this.board[2][0] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[0][1] == this.players[p] && this.board[1][1] == this.players[p] && this.board[2][1] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[0][2] == this.players[p] && this.board[1][2] == this.players[p] && this.board[2][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+
+ 		//check diagonal
+ 		if(this.board[2][0] == this.players[p] && this.board[1][1] == this.players[p] && this.board[0][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[0][0] == this.players[p] && this.board[1][1] == this.players[p] && this.board[2][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		
+ 			
+ 		
+
+ 	}
+ 	return "";
+ }
 
 
+//player class
 function playerClass(){
 	this.players = [];
 	this.playerTurnIndex = 0;
 }
 
+playerClass.prototype.addPlayer = function(str){
+	this.players.push(str);
+};
+
+playerClass.prototype.getPlayer = function(row,col){
+	return this.board[row][col];
+};
+
+
+//board class
 function boardClass(rows,cols){
 	this.rows = rows;
 	this.cols = cols;
@@ -20,3 +103,25 @@ function boardClass(rows,cols){
 		}
 	}
 }
+
+boardClass.prototype.makeMove = function(row,col){
+	
+	if(this.isValidMove(row,col)){
+		//update board array
+		this.board[row][col] = this.players[this.playerTurnIndex];
+		this.numOfMoves += 1;
+		
+
+		//notify "subscribers" to change!
+		for(var s=0; s<this.myChangeListeners.length;s++){
+			var thisView = this.myChangeListeners[s];
+			thisView.setCellText(row,col,this.players[this.playerTurnIndex]);
+		}
+		this.playerTurnIndex = this.numOfMoves % this.players.length;
+
+	}
+	else{
+		return "no move";
+	}
+};
+
