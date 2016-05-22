650fab32784a8c9561eb30c5a285b705258aebb9
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-05-22 14:13:00 -0400

    Update unitTesting.js

--- unitTesting.js
+++ unitTesting.js
@@ -11,10 +11,10 @@ for(var i=0; i<gameState.length;i++){
 	boardCopy.push(gameState[i]);
 
 }
-boardCopy[0].splice(0,1);
+/*boardCopy[0].splice(0,1);
 boardCopy[0].splice(0,1);
 console.log(boardCopy[0][0]);
-//console.log(boardCopy);//should return [[1,2,3],[4,5,6],[7,8,9]];
+//console.log(boardCopy);//should return [[1,2,3],[4,5,6],[7,8,9]];*/
 ///////////////////////////////////////////
 
 
@@ -36,7 +36,6 @@ function Model(rows,cols){
 };
 
 var m = new Model(3,3);
-//console.log(m);
 
 //////////////////////////////////////////////
 //Tested: Passed
@@ -51,7 +50,7 @@ m.addPlayer("O");
 /////////////////////////////////////////////
 //Tested: Passed
 Model.prototype.isValidMove = function(row,col){
-	if(this.board[row][col] == "" && row >= 0 && col >= 0){
+	if(this.board[row][col] == ""){
 		return true;
 	}
 	else {
@@ -72,103 +71,99 @@ Model.prototype.makeMove = function(row,col){
 		//update board array
 		this.board[row][col] = this.players[this.playerTurnIndex];
 		this.numOfMoves += 1;
-		console.log('got here');
-		if(this.playerTurnIndex === 0){
-			this.playerTurnIndex = 1;
+
+		if(this.playerTurnIndex !== this.players.length-1){
+			this.playerTurnIndex += 1;
 		}
 		else{
 			this.playerTurnIndex = 0;
-		} 
+		}
+	}
+	else{
+		return false;
 	}
 };
+
  
- //m.makeMove(0,0);
- //console.log(m.board[0][0]);
- //console.log(m.playerTurnIndex);
+ 
+ m.board[1][1] = "X";
+ console.log(m.board[1][1]);
+//console.log(m.isValidMove(0,1));
+ //console.log(m.board);
+ //console.log(m.isValidMove(0,0));
 //////////////////////////////////////////////
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Tested: Failed
- Model.prototype.playerWin = function(){
+//Tested: Passed
+Model.prototype.playerWin = function(){
 
  	for(var p=0;p<this.players.length;p++){
+ 		//check horizontal
+ 		if(this.board[0][0] == this.players[p] && this.board[0][1] == this.players[p] && this.board[0][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[1][0] == this.players[p] && this.board[1][1] == this.players[p] && this.board[1][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[2][0] == this.players[p] && this.board[2][1] == this.players[p] && this.board[2][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+
+ 		//check vertical
+ 		if(this.board[0][0] == this.players[p] && this.board[1][0] == this.players[p] && this.board[2][0] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[0][1] == this.players[p] && this.board[1][1] == this.players[p] && this.board[2][1] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[0][2] == this.players[p] && this.board[1][2] == this.players[p] && this.board[2][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+
+ 		//check diagonal
+ 		if(this.board[2][0] == this.players[p] && this.board[1][1] == this.players[p] && this.board[0][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
+ 		if(this.board[0][0] == this.players[p] && this.board[1][1] == this.players[p] && this.board[2][2] == this.players[p]){
+ 			return this.players[p];
+ 		}
  		
- 		for(var i=0;i<this.rows;i++){
- 			//check horizontal
- 			//Tested: Passed
- 			if(this.board[i][0] == this.players[p] && this.board[i][1] == this.players[p] && this.board[i][2] == this.players[p]){
- 				//winner
- 				return this.players[p];
- 				break;
- 			}
- 			//check vertical
- 			//Tested: Passed	
-			if(this.board[0][i] == this.players[p] && this.board[1][i] == this.players[p] && this.board[2][i]){
-				return this.players[p];
-				break;
-			}
-		}
-		//check diagonal
-		//Tested: Passed 
-		if(this.board[0][0] == this.players[p] && this.board[1][1] == this.players[p] && this.board[2][2] == this.players[p]){
-			return this.players[p];
-			break;
-		}
-		if(this.board[2][0] == this.players[p] && this.board[1][1] == this.players[p] && this.board[0][2] == this.players[p]){
-			return this.players[p];
-			break;
-		}
+ 			
+ 		
+
  	}
- 	return false;
- };
+ 	return "";
+ }
 
 
-/*m.board[0][2] = m.players[0];
-m.board[1][2] = m.players[1];
-m.board[2][2] = m.players[0];*/
-
-
-m.board[0][0] = m.players[1];
-m.board[0][1] = m.players[0];
-m.board[0][2] = m.players[1];
-
-m.board[1][0] = m.players[1];
-m.board[1][1] = m.players[0];
-m.board[1][2] = m.players[0];
-
-m.board[2][0] = m.players[0];
-m.board[2][1] = m.players[1];
-m.board[2][2] = m.players[1];
 //console.log(m.playerWin());
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////
-//Tested:
+//Tested: Passed
 Model.prototype.isDraw = function(){
 	var check = this.playerWin();
-	if(this.numOfMoves = 9 && (check === false)){
+	if((this.numOfMoves === 9) && (check === "")){
 		return true;
 	}
 	else{
 		return false;
 	}
 };
-m.board[0][0] = m.players[1];
-m.board[0][1] = m.players[0];
-m.board[0][2] = m.players[1];
-
-m.board[1][0] = m.players[1];
-m.board[1][1] = m.players[0];
+/*m.board[0][0] = m.players[0];
+m.board[0][1] = m.players[1];
+m.board[0][2] = m.players[0];
+//
+m.board[1][0] = m.players[0];
+m.board[1][1] = m.players[1];
 m.board[1][2] = m.players[0];
-
-m.board[2][0] = m.players[0];
-m.board[2][1] = m.players[1];
+//
+m.board[2][0] = m.players[1];
+m.board[2][1] = m.players[0];
 m.board[2][2] = m.players[1];
-
-m.numOfMoves += 9;
-
-
+m.numOfMoves = 9;
+*/
 //console.log(m.isDraw()); //return true
 /////////////////////////////////////////
 
@@ -186,16 +181,3 @@ Model.prototype.getPlayer = function(row,col){
 	return this.board[row][col];
 };
 ///////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
